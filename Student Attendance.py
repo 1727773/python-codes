6dddd4055b86a5f62eeba7f0a834361fa310519c
1727773 <139542283+1727773@users.noreply.github.com>
2026-09-23 12:37:58 +0530

    Add files via upload

---
+++ Student Attendance.py
@@ -0,0 +1,44 @@
+# Student Attendance
+
+pin = 2500039094
+branch = "CSE"
+name = "Dheeraj"
+
+subject1 = 100
+subject2 = 40
+subject3 = 29
+subject4 = 50
+subject5 = 67
+subject6 = 50
+subject7 = 67
+subject8 = 25
+subject9 = 58
+subject10 = 67
+subject11 = 50
+subject12 = 60
+subject13 = 34
+subject14 = 50
+
+average = (subject1+subject2+subject3+subject4+subject5+subject6+subject7+subject8+subject9+subject10+subject11+subject12+subject13+subject14) / 14
+
+print("PIN:", pin)
+print("Branch:", branch)
+print("Name:", name)
+
+print("25CS2101L:", subject1)
+print("25CS2101L:", subject2)
+print("25CS2102L:", subject3)
+print("25CS2102L:", subject4)
+print("25MT1306L:", subject5)
+print("25MT1306L:", subject6)
+print("25SC2006L:", subject7)
+print("25SC2006L:", subject8)
+print("25AD2102L:", subject9)
+print("25AD2102L:", subject10)
+print("25AD2102L:", subject11)
+print("25EC2206L:", subject12)
+print("25EC2206L:", subject13)
+print("25BR3001L:", subject14)
+
+print("Attendance:", subject1, subject2, subject3, subject5, subject6, subject7, subject8, subject9, subject10, subject11, subject12, subject13, subject14)
+print("Average:", average, "%")
